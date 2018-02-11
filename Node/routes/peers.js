d88e3e748211ad5f2377ed53baceb8126b57bc28
IskraNikolova <air_iskra@yahoo.com>
2018-02-11 18:45:39 +0200

    fix bugs

--- Node/routes/peers.js
+++ Node/routes/peers.js
@@ -3,6 +3,9 @@ const router = new express.Router()
 const WebSocket = require("ws")
 const index = require('./../index')
 
+//set HTTP_PORT=3001 && set P2P_PORT=6001 && npm start
+//set HTTP_PORT=3002 && set P2P_PORT=6002 && set PEERS=http://localhost:6001 && npm start
+
 //GET Peers
 router.get('/', (req, res) => {
    res.send(index.sockets.map(s => s._socket.remoteAddress + ':' + s._socket.remotePort));
