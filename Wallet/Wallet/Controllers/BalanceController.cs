148d22b0dbc17e9497a8c030e9d658bff1739b57
IskraNikolova <air_iskra@yahoo.com>
2018-02-20 16:48:25 +0200

    fix balance

--- Wallet/Wallet/Controllers/BalanceController.cs
+++ Wallet/Wallet/Controllers/BalanceController.cs
@@ -2,9 +2,17 @@ namespace Wallet.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
     using Wallet.Models.BindingModels;
+    using Wallet.Services.Interfaces;
 
     public class BalanceController : Controller
     {
+        private readonly IHttpRequestService httpRequestService;
+
+        public BalanceController(IHttpRequestService httpRequestService)
+        {
+            this.httpRequestService = httpRequestService;
+        }
+
         [HttpGet]
         public IActionResult AccountBalance()
         {
@@ -15,8 +23,12 @@ namespace Wallet.Controllers
         [HttpPost]
         public IActionResult AccountBalance(AcountBalanceBm bm)
         {
-            bm.Info = bm.Address;
-            return View(bm);
+            var address = bm.Address;
+            var blockChainNode = bm.BlockChainNode;
+            string resUrl = $"{blockChainNode}/balance/{address}/confirmations/6";
+            var responce = this.httpRequestService.Get<object>(resUrl);
+             bm.Info = responce + "";
+             return View(bm);
         }
     }
 }
