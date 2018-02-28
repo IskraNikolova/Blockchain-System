024bbdcd645e27ceac8ab4d81cd5ed833fad40e3
IskraNikolova <air_iskra@yahoo.com>
2018-02-28 22:55:14 +0200

    update

---
+++ Wallet/MyWallet/MyWallet/MyWallet/Models/ViewModels/Balance.cs
@@ -0,0 +1,9 @@
+﻿namespace MyWallet.Models.ViewModels
+{
+    public class Balance
+    {
+        public int Confirmations { get; set; }
+
+        public double BalanceData { get; set; }
+    }
+}
