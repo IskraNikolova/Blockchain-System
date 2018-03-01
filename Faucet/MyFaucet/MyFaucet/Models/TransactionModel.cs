ada179eb165057a961692e5b7adc0b99ef131e72
IskraNikolova <air_iskra@yahoo.com>
2018-03-01 13:29:32 +0200

    update version

--- Faucet/MyFaucet/MyFaucet/Models/TransactionModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyFaucet.Models
-{
-    public class TransactionModel
-    {
-        public string Address { get; set; }
-
-        public string Url { get; set; }
-    }
-}
--- Faucet/MyFaucet/MyFaucet/Services/Interfaces/IHttpRequestService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyFaucet.Services.Interfaces
-{
-    using MyFaucet.Models;
-
-    public interface IHttpRequestService
-    {
-        ResponseModel Post(string resURL, SendTransactionBody data);
-    }
-}
