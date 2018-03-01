60ab8bee5b73a2166da21a35ca1945a11a03d508
IskraNikolova <air_iskra@yahoo.com>
2018-03-01 13:36:57 +0200

    fix faucet structure

---
+++ Faucet/MyFaucet/Models/TransactionModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFaucet.Models
+{
+    public class TransactionModel
+    {
+        public string Address { get; set; }
+
+        public string Url { get; set; }
+    }
+}
---
+++ Faucet/MyFaucet/Services/Interfaces/ITransactionsService.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFaucet.Services.Interfaces
+{
+    using MyFaucet.Models;
+
+    public interface ITransactionsService
+    {
+        SendTransactionBody CreateAndSignTransaction(string recipientAddress, int value, int fee,
+                              string iso8601datetime, string senderPrivKeyHex);
+    }
+}
