e8128de5c540ecd272d5b22b9bbd92d85bd2abcd
IskraNikolova <air_iskra@yahoo.com>
2018-03-02 14:34:37 +0200

    fix bug

--- BlockExplorer/BlockExplorer/Controllers/HomeController.cs
+++ BlockExplorer/BlockExplorer/Controllers/HomeController.cs
@@ -4,6 +4,7 @@
     using Microsoft.AspNetCore.Mvc;
     using BlockExplorer.Models;
     using BlockExplorer.Services.Interfaces;
+    using System;
 
     public class HomeController : Controller
     {
@@ -16,6 +17,7 @@
         public IActionResult Index()
         {
             BlocksInfoVm model = this.httpRequestService.Get<BlocksInfoVm>("http://localhost:5555/info");
+            Array.Reverse(model.Blocks);
             return View(model);
         }
 
